e7c5dd39e8becf20c0c0235a4901eb4fbb4bd77f
jml-student <student741@proton.me>
2024-05-28 13:22:25 +0200

    Add main html and javascript

--- script.js
+++ script.js
@@ -9,8 +9,18 @@ function addBookToLibrary() {
 }
 
 function displayLibrary() {
+    const libraryContainer = document.querySelector(".library-container");
+    libraryContainer.innerHTML = "";
     for (i = 0; i < myLibrary.length; i++) {
         let div = document.createElement("div");
+        div.setAttribute("class", "book-container")
         div.textContent = myLibrary[i];
+        libraryContainer.appendChild(div);
     }
 }
+
+const newBookButton = document.querySelector(".new-book");
+newBookButton.addEventListener("click", () => {
+    const dialog = document.querySelector("dialog");
+    dialog.show();
+})
